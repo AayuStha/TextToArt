6edae733dec67d1c9eac368954b41f401f517349
Aayush Shrestha <aayustha21@gmail.com>
2024-10-28 00:14:25 +0545

    resolved port issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-const port = 3000;
+const port = 8080;
 
 app.get('/', function (req, res) {
     res.render('index');
